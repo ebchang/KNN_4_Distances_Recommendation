6968ffea6acb3217c7df7a898db31653e4a6f226
Evan Chang <echang015@yahoo.com>
2018-05-29 23:22:54 -0500

    Update Movie_Clustering_L2.py

--- KNN/Movie_Clustering_L2.py
+++ KNN/Movie_Clustering_L2.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Feb  3 13:58:32 2016
-
 @author: evanchang
 """
 import re
@@ -114,10 +112,10 @@ rating_test_object_list = []
 
 # Getting the specifics for what is going to be used in the metric algorithms
 d = Dataset()
-d.get_users("/Users/evanchang/Desktop/ml-100k/u.user", user_object_list)
-d.get_items("/Users/evanchang/Desktop/ml-100k/u.item", item_object_list)
-d.get_ratings("/Users/evanchang/Desktop/ml-100k/u.data", rating_object_list)
-d.get_ratings("/Users/evanchang/Desktop/ml-100k/u1.test", rating_test_object_list)
+d.get_users("u.user", user_object_list)
+d.get_items("u.item", item_object_list)
+d.get_ratings("u.data", rating_object_list)
+d.get_ratings("u1.test", rating_test_object_list)
  
 n_users = len(user_object_list)
 n_items = len(item_object_list)
